c5d858a91fd0083d0a91ddb2df051fa44f0eef28
thush3304 <thush3304@gmail.com>
2021-07-25 18:53:00 +0000

    createupdate

--- create.py
+++ create.py
@@ -7,52 +7,52 @@ db.create_all()
 
 #Creates Farmers Entry
 farm1 = Farmers(
-    name = "Kesh"
-    farm = "Sunflower"
+    name = "Kesh",
+    farm = "Sunflower",
     age = 20
 )
 farm2 = Farmers(
-    name = "Thush"
-    farm = "Lavender"
+    name = "Thush",
+    farm = "Lavender",
     age = 22
 )
 #Creates goods entry in relation to farm1
 good = Goods(
-    equipment = "Tractor"
-    equipmenttype = "Vehicle"
-    vegetables = "Sawi"
-    vegenum = 20
-    fruits = "Mango"
-    fruitnum = 55
-    farmers_id = 1
+    equipment = "Tractor",
+    equipmenttype = "Vehicle",
+    vegetables = "Sawi",
+    vegenum = 20,
+    fruits = "Mango",
+    fruitnum = 55,
+    farmers_id = 1,
 )
 good2 = Goods(
-    equipment = ""
-    equipmenttype = ""
-    vegetables = "Chillies"
-    vegenum = 123
-    fruits = "Durian"
-    fruitnum = 64
-    farmers_id = 1
+    equipment = "",
+    equipmenttype = "",
+    vegetables = "Chillies",
+    vegenum = 123,
+    fruits = "Durian",
+    fruitnum = 64,
+    farmers_id = 1,
 )
 #goods addition for farmer2
 good3 = Goods(
-    equipment = "Fork"
-    equipmenttype = "Tools"
-    vegetables = "Celery"
-    vegenum = 18
-    fruits = "Apples"
-    fruitnum = 5
-    farmers_id = 2
+    equipment = "Fork",
+    equipmenttype = "Tools",
+    vegetables = "Celery",
+    vegenum = 18,
+    fruits = "Apples",
+    fruitnum = 5,
+    farmers_id = 2,
 )
 
 good4 = Goods(
-    equipment = "Spade"
-    equipmenttype = "Tools"
-    vegetables = "Lettuce"
-    vegenum = 10
-    fruits = "Oranges"
-    fruitnum = 23
+    equipment = "Spade",
+    equipmenttype = "Tools",
+    vegetables = "Lettuce",
+    vegenum = 10,
+    fruits = "Oranges",
+    fruitnum = 23,
     farmers_id = 2
 )
 #Adds all changes onto the database
