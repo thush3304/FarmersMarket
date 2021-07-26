b001cfb3a144c2bf9fa6b9877950550a9222baa6
thush3304 <thush3304@gmail.com>
2021-07-26 01:34:26 +0000

    test

---
+++ tests/testunit.py
@@ -0,0 +1,114 @@
+from application import app, db
+from application.models import Farmers, Goods
+from flask import url_for
+from flask_testing import TestCase
+
+
+class TestBase(TestCase):
+
+    def create_app(self):
+        app.config.update(
+            SQLALCHEMY_DATABASE_URI="sqlite:///",
+            SECRET_KEY="TEST_SECRET_KEY",
+            DEBUG=True,
+            WTF_CSRF_ENABLED=False
+        )
+
+        return app
+
+    
+    def setUp(self):
+        
+        #Destroys previous database and creates a new one
+        db.drop_all()
+        db.create_all()
+
+        #Creates a Frankendama database entry
+        farmer = Farmers(
+            name='Test',
+            farm='TestFarm',
+            age='25',
+        )
+
+        db.session.add(farmer)
+
+        #Creates entries in the Company database relating to the Frankendama entry above
+        good = Goods(
+        equipment = "Tractor",
+        equipmenttype = "Vehicle",
+        vegetables = "Sawi",
+        vegenum = 20,
+        fruits = "Mango",
+        fruitnum = 55,
+        farmers_id = 1,
+        )    
+        good2 = Goods(
+        equipment = "Spade",
+        equipmenttype = "",
+        vegetables = "Chillies",
+        vegenum = 123,
+        fruits = "Durian",
+        fruitnum = 64,
+        farmers_id = 1,
+        )
+        db.session.add(good)
+        db.session.add(good2)
+        db.session.commit()
+
+    #Destroys database after running tests
+    def tearDown(self):
+
+        db.session.remove()
+        db.drop_all()
+
+#Tests the user is able to reach each page
+class TestViews(TestBase):
+
+    def test_home_get(self):
+        response = self.client.get(url_for('home'))
+        self.assertEqual(response.status_code,200)
+        
+    def test_create_get(self):
+        response = self.client.get(url_for('create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_get(self):
+        response = self.client.get(url_for('update', id=1))
+        self.assertEqual(response.status_code, 200)
+
+#Tests reading data from the homepage
+class TestRead(TestBase):
+
+    def test_read(self):
+        response = self.client.get(url_for('home'))
+        self.assertIn(b'Test', response.data)
+        self.assertIn(b'TestFarm', response.data)
+        self.assertIn(b'25', response.data)
+        self.assertIn(b'Tractor', response.data)
+
+#Tests the update fucntion of the flask app
+class TestUpdate(TestBase):
+
+    def test_update(self):
+        response = self.client.post(
+            url_for('update', id=1),
+            follow_redirects= True,
+            data = dict(
+                name="Jules"
+                
+            )
+        )
+
+        self.assertIn(b'Jules',response.data)
+
+#Tests the delete function of the flask app
+class TestDelete(TestBase):
+
+    def test_delete(self):
+             
+        response = self.client.get(
+        url_for('delete', id=1),
+        follow_redirects=True
+        )
+
+        assert "Test" not in response.data.decode()
